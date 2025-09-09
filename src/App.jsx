f5aa46b45305bc2720e4fc7d66b3abaf2da07f60
akhdanfarrosrenoldi <renoldiakhdanfarros@gmail.com>
2025-09-09 17:27:48 +0700

    feat: update App component layout and styling for improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,224 +3,66 @@ function App() {
     <>
       <div class="ml-32">
         <div class="p-12">
-          <div class="grid grid-cols-3 gap-4 mb-4">
-            <div class="flex flex-col justify-center h-24 rounded-sm bg-[#fafafa]">
-              <p class="text-gray-500">Dry Docks No</p>
-              <p>SEPT2020/DD1</p>
+          <div class="h-28 mb-4 bg-[#29a1ff]">
+            <div class="px-3 py-5">
+              <p class="text-xs font-semibold text-white">OCEAN STAR</p>
+              <p class="text-2xl font-semibold text-white">SEPT2020/DD1</p>
+              <p class="text-sm text-[#cce9ff]">DD Required change BWT</p>
             </div>
-            <div class="flex flex-col justify-center h-24 rounded-sm bg-[#fafafa]">
-              <p class="text-gray-500">Description</p>
-              <p>DD Required to change BWT</p>
-            </div>
-            <div class="flex flex-col justify-center h-24 rounded-sm bg-[#fafafa]">
-              <p class="text-gray-500">Company</p>
-              <p>UHC Pvt. Ltd.</p>
-            </div>
-            <div class="flex flex-col justify-center h-24 rounded-sm bg-[#fafafa]">
-              <p class="text-gray-500">Account Code</p>
-              <p>ABC-123</p>
-            </div>
-            <div class="flex flex-col justify-center h-24 rounded-sm bg-[#fafafa]">
-              <p class="text-gray-500">Responsible Rank</p>
-              <p>Roshan Ahluwalia/CE</p>
-            </div>
-            <div class="flex flex-col justify-center h-24 rounded-sm bg-[#fafafa]">
-              <p class="text-gray-500">Budget</p>
-              <p>200,000$</p>
+            <div class="fixed right-50 top-21">
+              <button class="bg-white hover:bg-gray-200 text-[#29a1ff] text-sm py-2 px-4 rounded">
+                Start Dry Dock
+              </button>
             </div>
           </div>
-          <div class="flex items-center justify-center h-48 mb-4 rounded-sm bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg
-                class="w-3.5 h-3.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 1v16M1 9h16"
-                />
-              </svg>
-            </p>
-          </div>
-          <div class="grid grid-cols-2 gap-4 mb-4">
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+
+          <div class="grid grid-cols-3">
+            <div class="flex flex-col justify-center h-12 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Dry Docks No</p>
+              <p class="px-3">SEPT2020/DD1</p>
             </div>
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+            <div class="flex flex-col justify-center h-12 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Description</p>
+              <p class="px-3">DD Required to change BWT</p>
             </div>
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+            <div class="flex flex-col justify-center h-12 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Company</p>
+              <p class="px-3">UHC Pvt. Ltd.</p>
             </div>
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+            <div class="flex flex-col justify-center h-24 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Account Code</p>
+              <p class="px-3">ABC-123</p>
+            </div>
+            <div class="flex flex-col justify-center h-24 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Responsible Rank</p>
+              <p class="px-3">Roshan Ahluwalia/CE</p>
+            </div>
+            <div class="flex flex-col justify-center h-24 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Budget</p>
+              <p class="px-3">200,000$</p>
+            </div>
+            <div class="flex flex-col justify-center h-12 bg-[#fafafa]">
+              <p class="text-gray-500 px-3">Budget</p>
+              <p class="px-3">200,000$</p>
             </div>
           </div>
-          <div class="flex items-center justify-center h-48 mb-4 rounded-sm bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg
-                class="w-3.5 h-3.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 1v16M1 9h16"
-                />
-              </svg>
-            </p>
-          </div>
-          <div class="grid grid-cols-2 gap-4">
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+
+          <div class="mt-4 grid grid-cols-3">
+            <div class="flex flex-col justify-center h-12 bg-white">
+              <p class="text-gray-500 px-3">Planned Start Date</p>
+              <p class="px-3">01/01/2020</p>
             </div>
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+            <div class="flex flex-col justify-center h-12 bg-white">
+              <p class="text-gray-500 px-3">Planned End Date</p>
+              <p class="px-3">01/01/2020</p>
             </div>
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+            <div class="flex flex-col justify-center h-12 bg-white">
+              <p class="text-gray-500 px-3">Actual Start Date</p>
+              <p class="px-3">01/01/2020</p>
             </div>
-            <div class="flex items-center justify-center rounded-sm bg-gray-50 h-28 dark:bg-gray-800">
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  class="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
-              </p>
+            <div class="flex flex-col justify-center h-24 bg-white">
+              <p class="text-gray-500 px-3">Actual End Date</p>
+              <p class="px-3">01/01/2020</p>
             </div>
           </div>
         </div>
